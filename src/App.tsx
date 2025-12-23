163b195a149600173cf8bb7d63a1f420ce7aacd8
Yehia Aly <yehia.z.aly@gmail.com>
2025-12-23 11:10:20 +0200

    init

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import BgColor from "./components/BgColor";
+
+function App() {
+  return (
+    <>
+      <BgColor />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/BgColor.tsx
@@ -0,0 +1,72 @@
+import { useEffect, useState } from "react";
+import { Button } from "@/components/ui/button";
+
+const BgColor = () => {
+  const [colorState, setcolorState] = useState<string>("hex");
+  const [color, setColor] = useState<string>("#FFFFF");
+
+  function generateRandomHexColor(): void {
+    const randomColorInt = Math.floor(Math.random() * 0xffffff);
+    let hexColor = randomColorInt.toString(16);
+    hexColor = hexColor.padStart(6, "0");
+
+    return setColor(`#${hexColor.toUpperCase()}`);
+  }
+
+  function generateRandomRgbColor(): void {
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+
+    return setColor(`rgb(${r}, ${g}, ${b})`);
+  }
+
+  function handleSelection(): void {
+    if (colorState === "rgb") {
+      generateRandomRgbColor();
+    } else if (colorState === "hex") {
+      generateRandomHexColor();
+    }
+  }
+
+  useEffect(() => {
+    handleSelection();
+  }, [colorState]);
+
+  return (
+    <div>
+      <div className="buttons mx-auto my-1.5 flex w-[70%] justify-center gap-x-3.5">
+        <Button
+          onClick={() => {
+            setcolorState("hex");
+          }}
+          variant="secondary"
+          className="px-8 py-4"
+        >
+          Create HEX Color
+        </Button>
+        <Button
+          onClick={() => {
+            setcolorState("rgb");
+          }}
+          variant="secondary"
+          className="px-8 py-4"
+        >
+          Create RGB Color
+        </Button>
+        <Button onClick={handleSelection} className="px-8 py-4">
+          Generate Random Color
+        </Button>
+      </div>
+      <div
+        style={{ backgroundColor: color }}
+        className="color-canvas text-content mt-10 flex h-96 max-h-96 max-w-full flex-col items-center justify-center gap-2 rounded-2xl text-xl font-semibold text-white"
+      >
+        <h1>{colorState === "hex" ? "HEX Color" : "RGB Color"}</h1>
+        <h1>{color}</h1>
+      </div>
+    </div>
+  );
+};
+
+export default BgColor;
